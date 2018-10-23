d4726bfe8ac2e3490b80cd2cb930e3de871e0865
Adam Giegel <adam.giegel@gmail.com>
2018-10-23 17:58:17 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,40 +1,58 @@
-def dictionary 
-  replace = {
-    "hello" => "hi",
-    "to" => "2",
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&",
-    "be" => "b"
-  }
+# Write your code here.
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
 end
 
-def word_substituter(tweet_string)
-    tweet_string.split(" ").collect do |i|
-      if dictionary.keys.include?(i)
-        i = dictionary[i]
-      else
-        i
+# You can use regex or, check out the solution below that
+# def word_substituter(tweet)
+#   tweet = tweet.dup
+#   dictionary.each do |word, replacement|
+#     if tweet.include?(word)
+#       tweet.gsub!(/\b#{word}\b/i, replacement)
+#     end
+#   end
+#   tweet
+# end
+
+
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
     end
   end.join(" ")
 end
 
-def bulk_tweet_shortener(tweet_array)
-  tweet_array.collect do |i|
-    puts word_substituter(i)
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
   end
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length > 140 ? word_substituter(tweet) : tweet
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
   end
 end
 
-def shortened_tweet_truncator(tweets)
-  tweets = tweets[0..140] + '...' if tweets.length > 140
-  tweets
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + '...'
+  else
+    tweet
+  end
 end
